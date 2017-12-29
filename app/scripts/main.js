0d6aa1037b5c5460a625171cb57f426b86ea5c76
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2017-12-29 21:00:07 +0200

    added dynamic on scroll

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,40 @@
 $(document).ready(() => {
-  $('a').click(clickA);
   $('.header__btn').click(clickHeaderBtn);
+
+  var $scrollStatus = $('.scroll-status');
+
+  let top = 70;
+  if ($('body').width() > 767) {
+    top = 90;
+  }
+
+  $('.header__nav').navScroll({
+    mobileDropdown: true,
+    mobileBreakpoint: 768,
+    scrollSpy: true,
+    navHeight: top,
+    onScrollStart: () => {
+      $scrollStatus.show();
+      $scrollStatus.text('Started scrolling');
+    },
+    onScrollEnd: () => {
+      $scrollStatus.text('Scrolling ended');
+      setTimeout(() => {
+        $scrollStatus.fadeOut(200);
+      }, 1000);
+    }
+  });
+
+  $('.header__nav').on('click', '.header__a', (e) => {
+    e.preventDefault();
+    $('.header__nav ul').slideToggle('fast');
+    $(e.target).closest('body').removeClass('header__btn_active');
+    $(e.target).closest('body').find('.header__btn').toggleClass('header__btn_active');
+  });
+
 });
 
-$(window).on('load resize scroll', function() {
+$(window).on('load resize scroll', () => {
   headerSticky();
 });
 
@@ -13,41 +44,10 @@ function clickHeaderBtn() {
   $(this).closest('body').toggleClass('header__btn_active');
 }
 
-function clickA(event) {
-  const link = event.currentTarget.getAttribute('href');
-  if(link.length > 1 && link.indexOf('#') === 0) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    navbar($(this));
-
-    const section = document.querySelector(link);
-    if(section) {
-      let top = 70;
-      if($(section).closest('body').width() > 767) {
-        top = 90;
-      }
-      $('html, body').animate({
-        scrollTop: $(link).offset().top - top
-      }, 500);
-      window.location.hash = link;
-      return false;
-    }
-  }
-}
-
-function navbar(active) {
-  if(active.hasClass('header__a')) {
-    active.closest('li').addClass('header__li_active').siblings('li').removeClass('header__li_active');
-    active.closest('body').removeClass('header__btn_active');
-    active.closest('body').find('.header__btn').toggleClass('header__btn_active');
-  }
-}
-
 function headerSticky() {
   const sticky = $('.header');
   const scroll = $(window).scrollTop();
-  if(scroll >= 1) {
+  if (scroll >= 1) {
     sticky.addClass('header_fixed');
   } else {
     sticky.removeClass('header_fixed');
