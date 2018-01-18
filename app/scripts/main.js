70fec51ee370259ed8509ad5199eb0ffe211d6f8
Tama <github@tama.co.nz>
2018-01-18 17:06:39 +0100

    Working on mobile view

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,28 @@
 let headerButton;
+let nav;
+let navItems;
 
 function toggleMenu() {
-  if(headerButton.className.match(/header__btn_active/)) {
-    document.body.className = document.body.className.replace(/ ?header__btn_active/, '');
-    headerButton.className = headerButton.className.replace(/ ?header__btn_active/, '');
-  } else {
-    headerButton.className += ' header__btn_active';
-    document.body.className += ' header__btn_active';
-  }
+    if (headerButton.className.match(/nav__button--active/)) {
+        nav.className = nav.className.replace(/ ?nav__items--active/, '');
+        headerButton.className = headerButton.className.replace(/ ?nav__button--active/, '');
+    } else {
+        headerButton.className += ' nav__button--active';
+        nav.className += ' nav__items--active';
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  headerButton = document.getElementsByClassName('js-header-button')[0];
-  headerButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    toggleMenu();
-  });
+    headerButton = document.getElementsByClassName('js-nav-button')[0];
+    nav = document.getElementsByClassName('js-nav')[0];
+    headerButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    // Close menu when link clicked
+    navItems = document.getElementsByClassName('js-nav-item');
+    for (let i = 0; i < navItems.length; i++) {
+        navItems[i].addEventListener('click', toggleMenu, false);
+    }
 });
